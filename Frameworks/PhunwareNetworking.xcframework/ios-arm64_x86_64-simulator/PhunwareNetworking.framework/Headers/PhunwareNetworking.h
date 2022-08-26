9a78882a0b09859088d91d70ac85f3da70489365
jenkins <hostmaster@phunware.com>
2022-08-26 02:30:43 -0700

    Release version 1.1.0. See CHANGELOG for details.

---
+++ Frameworks/PhunwareNetworking.xcframework/ios-arm64_x86_64-simulator/PhunwareNetworking.framework/Headers/PhunwareNetworking.h
@@ -0,0 +1,19 @@
+//
+//  PhunwareNetworking.h
+//  PhunwareNetworking
+//
+//  Created by Troy Stump on 12/1/20.
+//  Copyright © 2020 Phunware, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhunwareNetworking.
+FOUNDATION_EXPORT double PhunwareNetworkingVersionNumber;
+
+//! Project version string for PhunwareNetworking.
+FOUNDATION_EXPORT const unsigned char PhunwareNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhunwareNetworking/PublicHeader.h>
+
+
